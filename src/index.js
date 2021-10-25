a1191e65a560838b9268b473d79f4439276da7be
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-25 06:42:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
