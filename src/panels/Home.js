0b1e7cd9d8d209b7cc483ca039f5c3d3c952169e
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-25 07:33:05 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -16,17 +16,6 @@ const Home = props => (
 	</Panel>
 );
 
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
+
 
 export default Home;
