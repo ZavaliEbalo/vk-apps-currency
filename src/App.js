d5e1d8ae051d4a8a393b9b33ee9a75bea6859dc3
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-25 07:56:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
+import React from "react";
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+import { Stack, Page, Tabbar } from "vkui-navigator/dist";
 
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+import Panel1 from "./panels/Panel1";
+import Panel2 from "./panels/Panel2";
+import Panel3 from "./panels/Panel3";
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+import Modal1 from "./panels/Modal1";
+import Modal2 from "./panels/Modal2";
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+import Icon24Services from "@vkontakte/icons/dist/24/services";
+import Icon24Flash from "@vkontakte/icons/dist/24/flash";
+import Panel4 from "./panels/Panel4";
 
-	return (
-		<AdaptivityProvider>
-			<AppRoot>
-				<View activePanel={activePanel} popout={popout}>
-					<Home id='home' fetchedUser={fetchedUser} go={go} />
-					<Persik id='persik' go={go} />
-				</View>
-			</AppRoot>
-		</AdaptivityProvider>
-	);
-}
+const App = () => (
+	<Stack
+		activePage="main"
+		modals={[
+			<Modal1 id="modal1"/>,
+			<Modal2 id="modal2" title="Название из title"/>
+		]}
+	>
+		<Tabbar
+			id="main"
+			activeStory="home"
+		>
+			<Page
+				id="home"
+				title="Основы"
+				activePanel="panel1"
+				icon={<Icon24Services/>}
+			>
+				<Panel1 id="panel1"/>
+				<Panel2 id="panel2"/>
+			</Page>
+			<Page
+				id="settings"
+				title="Модальные окна"
+				activePanel="panel3"
+				icon={<Icon24Flash/>}
+			>
+				<Panel3 id="panel3"/>
+			</Page>
+		</Tabbar>
+		<Page id="page2" activePanel="panel4">
+			<Panel4 id="panel4"/>
+		</Page>
+	</Stack>
+);
 
 export default App;
